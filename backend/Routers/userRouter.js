29ddd9c09434121f710467f23a23a49c9fdbb486
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2025-09-05 15:40:13 +0530

    Add JWT authentication middleware and import jsonwebtoken in the main application

--- backend/Routers/userRouter.js
+++ backend/Routers/userRouter.js
@@ -1,8 +1,10 @@
 import express from 'express';
 import { createUser } from '../controllers/userController.js';
+import { loginUser } from '../controllers/userController.js';
 
 const userRouter = express.Router();
 
 userRouter.post('/', createUser);
+userRouter.post('/login', loginUser);
 
 export default userRouter;
--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import bodyParser from "body-parser";
 import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
 import userRouter from "./Routers/userRouter.js";
 
 // Load environment variables
@@ -10,6 +11,24 @@ dotenv.config({ path: '../.env' });
 const app = express();
 
 app.use(bodyParser.json());
+
+// JWT Authentication Middleware
+app.use((req, res, next) => {
+  const value = req.headers['authorization'];
+  if (value != null) {
+    const token = value.replace("Bearer ", "");
+    jwt.verify(token, "secret", (err, decoded) => {
+      if (decoded == null) {
+        return res.status(401).json({ error: "Unauthorized" });
+      } else {
+        req.user = decoded;
+        next();
+      }
+    });
+  } else {
+    next();
+  }
+});
 
 const connectionString = process.env.DATABASE_URL || "mongodb+srv://course_platform:course123@cluster0.beepekj.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0m";
 
